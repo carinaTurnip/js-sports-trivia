f08721569a77a40c3e7f9cac34b913d5a6d60998
Carina <higginscarina@gmail.com>
2024-10-26 16:32:50 -0500

    progress on exam

--- site.js
+++ site.js
@@ -9,10 +9,21 @@ const nextQuestionElement = document.querySelector('#nextQuestion')
 
 // IIFE (so we can use async/await)
 ;(async () => {
+	
 
-	// todo: create your "getNextQuestion" function
+	const getNextQuestion = async () => { 
+		//fetch questions and format question and answers in JSON
+		const pulled = await fetch('https://opentdb.com/api.php?amount=1');
+		const json = await pulled.json();
+		const { question, correct_answer: correct, incorrect_answers: incorrect } = json.results[0]
+		const answers = shuffle([ ...incorrect, correct ])
+		return { question, answers, correct }
+	};
 
 	// todo: create your "renderQuestion" function
+	const renderQuestion = async () => {
+
+	};
 
 	// todo: add the event listener to the "nextQuestion" button
 
